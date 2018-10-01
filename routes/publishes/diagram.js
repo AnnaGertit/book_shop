e0bb91500b345dbd5963012d478f47e84a7aa034
Ivan Sehliuk <is@bam-boo.eu>
2018-10-01 18:08:33 +0300

    Draws a chart

---
+++ routes/publishes/diagram.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dbHelper = require('lib/db_helper');
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+
+  let list;
+  try {
+    list = await dbHelper.publishes.list();
+    console.log(list);
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.render('iface/publishes/diagram', {list: list});
+});
+
+module.exports = router;
